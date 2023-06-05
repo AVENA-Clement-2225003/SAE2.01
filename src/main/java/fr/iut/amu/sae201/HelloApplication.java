bb919f3f82b93e240d7eb1e3822c41a79f78b36c
Michael KERGUELEN <clement@antranet.fr>
2023-06-05 18:39:17 +0200

    Début de la création du FXML (changement à vérifier parce que ils n'ont pas pu être testés)

--- src/main/java/fr/iut/amu/sae201/HelloApplication.java
+++ src/main/java/fr/iut/amu/sae201/HelloApplication.java
@@ -12,13 +12,13 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
 
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
 
-    public static void main(String[] args) {
+    public static void Main(String[] args) {
         launch();
     }
 }
