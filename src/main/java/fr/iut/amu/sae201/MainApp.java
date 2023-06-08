fffc09e7599b1aece405344a3a03f1cd9129a6a7
Michael KERGUELEN <clement@antranet.fr>
2023-06-08 09:37:58 +0200

    Ajout de composants dans le Dashboard et création d'un style.css qui n'as pas pu etre encore testé

--- src/main/java/fr/iut/amu/sae201/MainApp.java
+++ src/main/java/fr/iut/amu/sae201/MainApp.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-
 public class MainApp extends Application {
 
     private Stage primaryStage;
